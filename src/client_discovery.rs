cc4ebb2c998a305458f0acef6cdd8613e79ad8a6
MuhiaKevin <archerkreiger@gmail.com>
2024-04-23 11:46:02 +0300

    Moved receiving clients functionality to lib.rs

--- src/client_discovery.rs
+++ src/client_discovery.rs
@@ -1,22 +1,12 @@
-use std::net::{SocketAddr, UdpSocket};
+use std::net::UdpSocket;
 use std::str::from_utf8;
-use std::sync::mpsc;
+use std::sync::mpsc::Sender;
+use crate::DuktoClientMessage;
 
 
 // custom type to hold the message and where the message comes from
-#[derive(Debug)]
-struct DuktoClientMessage {
-    message: String,
-    address: SocketAddr,
-}
-
-pub fn discover_clients() {
-    let (sender, reciever) = mpsc::channel();
-    let mut clients = std::collections::HashMap::new();
-
+pub fn discover_clients(client: Sender<DuktoClientMessage>)  {
     std::thread::spawn(move || {
-        let client = sender.clone();
-
         let udp_socket = UdpSocket::bind("0.0.0.0:4644")
             .expect("Failed to bind socket to port 4644");
 
@@ -40,24 +30,4 @@ pub fn discover_clients() {
             client.send(dc).unwrap();
         }
     });
-
-    // while let Ok(dukto_client)  = reciever.recv() {
-    //     let _ = &clients.insert(
-    //         dukto_client.message,
-    //         dukto_client.address,
-    //     );
-    //
-    //     println!("Result: {:?}\n\n", clients);
-    // }
-
-
-    // spawn threads that sends files, folders or text to other dukto clients
-    for dukto_client in reciever {
-        let _ = &clients.insert(
-            dukto_client.message,
-            dukto_client.address,
-        );
-
-        println!("Result: {:?}\n\n", clients);
-    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,46 @@ mod dukto_download;
 mod client_discovery;
 
 
+use std::sync::mpsc;
+use std::net::SocketAddr;
 use anyhow::Result;
 
+
+#[derive(Debug)]
+struct DuktoClientMessage {
+    message: String,
+    address: SocketAddr,
+}
+
+
 pub fn run() -> Result<()> {
-    client_discovery::discover_clients();
-    dukto_download::download()?;
+    let (sender, reciever) = mpsc::channel();
+    let mut clients = std::collections::HashMap::new();
+
+    client_discovery::discover_clients(sender);
+
+    // while let Ok(dukto_client)  = reciever.recv() {
+    //     let _ = &clients.insert(
+    //         dukto_client.message,
+    //         dukto_client.address,
+    //     );
+    //
+    //     println!("Result: {:?}\n\n", clients);
+    // }
+
+
+    // spawn threads that sends files, folders or text to other dukto clients
+    for dukto_client in reciever {
+        // 1. check if client is in map and add the client to hashmap if not in
+        // 2. if client not in hashmap, add the client to hash map and send the file/folder
+        let _ = &clients.insert(
+            dukto_client.message,
+            dukto_client.address,
+        );
+
+        println!("Result: {:?}\n\n", clients);
+    }
+
+    //dukto_download::download()?;
     Ok(())
 }
