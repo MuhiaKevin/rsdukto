914f0874e866ff40be7d81d6e7bcaaefb581f883
MuhiaKevin <archerkreiger@gmail.com>
2024-04-23 12:57:57 +0300

    send udp broadcast message

--- src/client_discovery.rs
+++ src/client_discovery.rs
@@ -1,12 +1,13 @@
 use std::net::UdpSocket;
+use std::time::Duration;
 use std::str::from_utf8;
 use std::sync::mpsc::Sender;
 use crate::DuktoClientMessage;
 
 
-// custom type to hold the message and where the message comes from
+
 pub fn discover_clients(client: Sender<DuktoClientMessage>)  {
-    std::thread::spawn(move || {
+    let handle1 = std::thread::spawn(move || {
         let udp_socket = UdpSocket::bind("0.0.0.0:4644")
             .expect("Failed to bind socket to port 4644");
 
@@ -30,4 +31,42 @@ pub fn discover_clients(client: Sender<DuktoClientMessage>)  {
             client.send(dc).unwrap();
         }
     });
+
+
+    let handle2 = std::thread::spawn(|| {
+        get_discovered_by_clients();
+
+    });
+
+    handle1.join().unwrap();
+    handle2.join().unwrap();
+}
+
+
+fn send_discover_message() -> Vec<u8>{
+    let mut header: Vec<u8> =  vec![1];
+    let message = b"Chifu at Kizunu (Rustlang)";
+
+    header.extend_from_slice(&message[..]);
+    header
+}
+
+
+fn get_discovered_by_clients() {
+    let msg = send_discover_message();
+
+    let socket = UdpSocket::bind("0.0.0.0:0").expect("couldn't bind to address");
+    socket.set_broadcast(true).expect("set_broadcast call failed");
+
+    // let broadcast_ip = Ipv4Addr::new(255, 255, 255, 255); 
+    // let broadcast_port = 4644; 
+    // let broadcast_socket_addr = SocketAddrV4::new(broadcast_ip, broadcast_port);
+
+
+    loop {
+        println!("Sending broad cast message");
+        // socket.send_to(&msg, broadcast_socket_addr).expect("couldn't send data");
+        socket.send_to(&msg, "255.255.255.255:4644").expect("couldn't send data");
+        std::thread::sleep(Duration::from_secs(2))
+    }
 }
